ed83dfa4b8a3b3456a2d9554e3164f23401d671d
armandfardeau <fardeauarmand@gmail.com>
2023-01-26 23:58:19 +0100

    Add question answerer

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from models.summarize import perform_summarize
 from models.sentiment_analysis import perform_sentiment_analysis
 from models.language_detection import perform_language_detection
+from models.question_answerer import perform_answerer
 from utils.translate import translate_request, perform_translate
 
 app = Flask(__name__)
@@ -16,7 +17,8 @@ def ping():
 def summarize():
     request_content = translate_request(request.get_json())
     response = perform_summarize(request_content['content'])
-    translated_response = perform_translate(response[0]["summary_text"], source_lang="EN", target_lang=request_content['lang'])
+    translated_response = perform_translate(response[0]["summary_text"], source_lang="EN",
+                                            target_lang=request_content['lang'])
     return jsonify(content=translated_response)
 
 
@@ -31,3 +33,11 @@ def sentiment_analysis():
 def language_detection():
     request_content = request.get_json()
     return jsonify(content=perform_language_detection(request_content['content']))
+
+
+@app.route('/question-answerer', methods=['POST'])
+def answer():
+    request_content = translate_request(request.get_json())
+    response = perform_answerer(request_content['content'])
+    translated_response = perform_translate(response["answer"], source_lang="EN", target_lang=request_content['lang'])
+    return jsonify(content=translated_response)
---
+++ models/question_answerer.py
@@ -0,0 +1,8 @@
+from transformers import pipeline
+
+
+def perform_question_answerer(content):
+    question = content['question']
+    knowledge = content['knowledge']
+    question_answerer = pipeline("question-answering", model="distilbert-base-uncased-distilled-squad")
+    return question_answerer(question=question, context=knowledge)
