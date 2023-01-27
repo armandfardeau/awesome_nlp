4a80b29ddd66bd04d480aaa21293b9b7e4d4b045
armandfardeau <fardeauarmand@gmail.com>
2023-01-27 02:19:51 +0100

    Fix model path

--- models/summarize.py
+++ models/summarize.py
@@ -2,5 +2,5 @@ from transformers import pipeline
 
 
 def perform_summarize(text):
-    summarizer = pipeline("summarization", model="./cache/facebook/bart-large-cnn")
+    summarizer = pipeline("summarization", model="./cache/bart-large-cnn")
     return summarizer(text, max_length=130, min_length=30, do_sample=False)
--- models/text_generation.py
+++ models/text_generation.py
@@ -3,5 +3,5 @@ from transformers import pipeline
 
 def perform_text_generation(text, max_length=50, do_sample="False"):
     sample = True if do_sample == "True" else False
-    text_generator = pipeline("text-generation", model="./cache/EleutherAI/gpt-j-6B")
+    text_generator = pipeline("text-generation", model="./cache/gpt-j-6B")
     return text_generator(text, max_length=max_length, do_sample=sample)
