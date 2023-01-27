796cc13f9d8c15a6239a2ba01604421f7ea91538
armandfardeau <fardeauarmand@gmail.com>
2023-01-27 02:46:42 +0100

    Use model from cache

--- models/image_classification.py
+++ models/image_classification.py
@@ -3,6 +3,6 @@ from PIL import Image
 import requests
 
 def perform_classify_image(url):
-    image_classifier = pipeline("image-classification", model="./cache/vit-base-patch16-224")
+    image_classifier = pipeline("image-classification", model="/app/cache/vit-base-patch16-224")
     image = Image.open(requests.get(url, stream=True).raw)
     return image_classifier(image)
--- models/question_answerer.py
+++ models/question_answerer.py
@@ -4,5 +4,5 @@ from transformers import pipeline
 def perform_question_answerer(content):
     question = content['question']
     knowledge = content['knowledge']
-    question_answerer = pipeline("question-answering", model="./cache/distilbert-base-uncased-distilled-squad")
+    question_answerer = pipeline("question-answering", model="/app/cache/distilbert-base-uncased-distilled-squad")
     return question_answerer(question=question, context=knowledge)
