9ede2a2508357fffe6ba463a898ccc90c2011a3f
armandfardeau <fardeauarmand@gmail.com>
2023-01-29 21:20:56 +0100

    Removes jsonify

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, request
 from utils.config import cache, config
 from models.summarize import perform_summarize
 from models.sentiment_analysis import perform_sentiment_analysis
@@ -8,17 +8,24 @@ from models.question_answerer import perform_question_answerer
 from models.text_generation import perform_text_generation
 from models.image_classification import perform_classify_image
 from utils.translate import translate_request, perform_translate
-from utils.pipelines_helper import preload_pipelines
+from utils.pipelines_helper import preload_pipelines, pipeline_file
 
 app = Flask(__name__)
 cache.init_app(app, config=config)
 if os.environ.get('PRELOAD_PIPELINES') == 'true':
     preload_pipelines()
 
+
 @cache.cached()
 @app.route('/ping')
 def ping():
-    return jsonify(content="pong")
+    return {"content": "pong"}
+
+
+@cache.cached()
+@app.route('/')
+def root():
+    return {"content": str(pipeline_file())}
 
 
 @app.route('/summarize', methods=['POST'])
@@ -27,20 +34,19 @@ def summarize():
     response = perform_summarize(request_content['content'])
     translated_response = perform_translate(response[0]["summary_text"], source_lang="en",
                                             target_lang=request_content['lang'])
-    return jsonify(content=translated_response)
+    return {"content": translated_response}
 
 
 @app.route('/sentiment-analysis', methods=['POST'])
 def sentiment_analysis():
     request_content = translate_request(request.get_json())
-    response = perform_sentiment_analysis(request_content['content'])
-    return jsonify(content=response)
+    return {"content": perform_sentiment_analysis(request_content['content'])}
 
 
 @app.route('/language-detection', methods=['POST'])
 def language_detection():
     request_content = request.get_json()
-    return jsonify(content=perform_language_detection(request_content['content']))
+    return {"content": perform_language_detection(request_content['content'])}
 
 
 @app.route('/question-answerer', methods=['POST'])
@@ -51,7 +57,7 @@ def answer():
     lang = question['lang']
     response = perform_question_answerer({"question": question['content'], "knowledge": knowledge['content']})
     translated_response = perform_translate(response["answer"], source_lang="en", target_lang=lang)
-    return jsonify(content=translated_response)
+    return {"content": translated_response}
 
 
 @app.route('/generate-text', methods=['POST'])
@@ -60,10 +66,10 @@ def generate():
     response = perform_text_generation(request_content['content'], request_content['max_length'])
     translated_response = perform_translate(response[0]["generated_text"], source_lang="en",
                                             target_lang=request_content['lang'])
-    return jsonify(content=translated_response)
+    return {"content": translated_response}
 
 
 @app.route('/classify-image', methods=['POST'])
 def classify_image():
     request_content = request.get_json()
-    return jsonify(content=perform_classify_image(request_content['content']))
+    return {"content": perform_classify_image(request_content['content'])}
